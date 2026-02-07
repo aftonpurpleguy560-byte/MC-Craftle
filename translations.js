80708d476e32ae5afd7cf991e94f512f5647c079
aftonpurpleguy560-byte <aftonpurpleguy560@gmail.com>
2026-02-08 01:40:22 +0300

    Create translations.js

---
+++ translations.js
@@ -0,0 +1,19 @@
+const i18n = {
+    tr: {
+        title: "MC CRAFTLE",
+        placeholder: "Eşya adı yaz...",
+        playButton: "OYUNA BAŞLA",
+        error: "Yanlış tarif!",
+        success: "Tebrikler, buldun!"
+    },
+    en: {
+        title: "MC CRAFTLE",
+        placeholder: "Type item name...",
+        playButton: "START GAME",
+        error: "Wrong recipe!",
+        success: "Congratulations, you found it!"
+    }
+};
+
+let currentLang = 'tr'; // Varsayılan dil
+
