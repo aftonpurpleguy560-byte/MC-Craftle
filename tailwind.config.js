2dc6e00029c7155914ec8c31b06aca74c10c105b
aftonpurpleguy560-byte <aftonpurpleguy560@gmail.com>
2026-02-08 00:44:46 +0300

    Add Tailwind CSS configuration file

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./script.js"],
+  theme: {
+    extend: {
+      colors: {
+        'mc-grey': '#c6c6c6',
+        'mc-green': '#5ea818',
+      }
+    },
+  },
+  plugins: [],
+}
+
