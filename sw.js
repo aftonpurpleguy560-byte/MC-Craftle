f0b5480247400e60988e40f18f7df98cad5a224d
aftonpurpleguy560-byte <aftonpurpleguy560@gmail.com>
2026-02-08 00:45:51 +0300

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,11 @@
+const CACHE_NAME = 'mc-craftle-v1';
+const assets = ['/', '/index.html', '/style.css', '/script.js', 'https://raw.githubusercontent.com/PrismarineJS/minecraft-assets/master/data/1.12/items.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
+
